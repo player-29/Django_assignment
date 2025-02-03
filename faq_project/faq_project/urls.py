840e84d01c6036b980474424bccb62a2833d91a7
ayushrathour <ayushrathour2910@gmail.com>
2025-02-03 10:45:29 +0530

    fix: add url patterns

--- faq_project/faq_project/urls.py
+++ faq_project/faq_project/urls.py
@@ -16,14 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from faq.views import FAQViewSet, home
 
-router = DefaultRouter()
-router.register(r'faqs', FAQViewSet, basename='faq')
 
 urlpatterns = [
-    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('faq.urls')),
 ]
